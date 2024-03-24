8e74c858476603290e262a421527a70aba3bea9e
AluminumAnalyzer <blueunicon@gmail.com>
2024-03-24 23:13:05 +0900

    Deployment Error resolved

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"8ERsc7s7o3abqCuyXB5LCns2484cgt7vACGJj3ABSE0=\"\n}"
--- app/about-us/about.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = {
-  title: 'About Us',
-};
-
-const AboutUs = () => {
-  return <div>About Us</div>;
-};
-
-export default AboutUs;
