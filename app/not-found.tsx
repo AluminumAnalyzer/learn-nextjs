73a9050e0c422bb6825c7559091698ffbc01bd8e
AluminumAnalyzer <blueunicon@gmail.com>
2024-03-24 23:21:56 +0900

    Deployment not-found Error resolved

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,14 +3,9 @@ import { usePathname, useRouter } from 'next/navigation';
 import { useCallback } from 'react';
 
 const ErrorPage = () => {
-  const pathname = usePathname();
-  const router = useRouter();
-  const back = useCallback(() => router.push('/'), []);
   return (
     <div>
-      <p>현재 경로: {pathname}</p>
-      <p>이 페이지는 존재 하지 않습니다.</p>
-      <button onClick={back}>메인 페이지로 이동...</button>
+      <h1>404 - Page Not Found</h1>
     </div>
   );
 };
